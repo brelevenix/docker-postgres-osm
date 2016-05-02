8b1af64342ecdb963db7d5c3a1a2759dd304f808
brelevenix <eric.debeau@gmail.com>
2016-05-02 22:28:16 +0200

    Update postgres-entry.sh

--- postgres-entry.sh
+++ postgres-entry.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 set -e
 
+# variable for the PG conf location because we're working on it so much
+PGCONF="/usr/share/postgresql/9.3/postgresql.conf.sample"
+
+# Settings to allow 2GB
+sed -i 's/#*kernel.shmmax = .*/kernel.shmmax = 2147483648/' /etc/sysctl.d/30-postgresql-shm.conf
+sed -i 's/#*kernel.shmall = .*/kernel.shmmax = 524288/' /etc/sysctl.d/30-postgresql-shm.conf
+sysctl -p # doesn't work, what's the sysctl command?
+
+sed -i 's/#*shared_buffers = .*/shared_buffers = 1GB/' $PGCONF
+# Set to about 50% of system memory
+sed -i 's/#*effective_cache_size = .*/effective_cache_size = 8GB/' $PGCONF
+
+# If you have the RAM, 1GB is good
+sed -i 's/#*maintenance_work_mem = .*/maintenance_work_mem = 256MB/' $PGCONF
+
+# More helps speed up rendering queries
+sed -i 's/#*work_mem = .*/work_mem = 32MB/' $PGCONF
+
+# Suggested parameters for bulk loading
+sed -i 's/#*checkpoint_segments = .*/checkpoint_segments = 256/' $PGCONF
+sed -i 's/#*checkpoint_completion_target = .*/checkpoint_completion_target = 0.9/' $PGCONF
+
+# Parameter tuning
+sed -i 's/#*random_page_cost = .*/random_page_cost = 2.0/' $PGCONF
+# On older PG versions increase cpu_tuple_cost to 0.05-0.10
+
+# Autovacuum tuning to minimize bloat
+sed -i 's/#*autovacuum_vacuum_scale_factor = .*/autovacuum_vacuum_scale_factor = 0.04/' $PGCONF
+sed -i 's/#*autovacuum_analyze_scale_factor = .*/autovacuum_analyze_scale_factor = 0.02/' $PGCONF
+
 gosu postgres postgres --single -jE <<-EOL
   CREATE USER "$OSM_USER";
 EOL
